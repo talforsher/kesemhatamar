6a001767c0672cfcf8a6d4473f826223bed25062
adikrief <44708302+adikrief@users.noreply.github.com>
2021-03-08 16:27:54 +0200

    Auto Deploy

--- edit.js
+++ edit.js
@@ -288,7 +288,8 @@ newImage.onchange = function () {
 
     // Define what happens on successful data submission
     XHR.addEventListener("load", function (event) {
-        document.getElementById('img').src = "main.webp"
+console.log(event.target.response)
+        document.getElementById('img').src = "img"+event.target.response+".webp"
     });
 
     // Define what happens in case of error
